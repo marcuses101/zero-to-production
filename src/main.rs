f519a17d0c856841f07be6a23782c01bc6ad391f
marcuses101 <mnjconnolly@gmail.com>
2023-06-17 15:08:23 -0400

    fix: init_script use bash

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 use zero_to_production::configuration::get_configuration;
 use zero_to_production::startup::run;
@@ -5,9 +6,10 @@ use zero_to_production::startup::run;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration");
-    let listener =
-        TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port)).expect(
-            &format!("Failed to bind to port: {}", configuration.application_port),
-        );
-    run(listener)?.await
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("failed to connect to Postgres");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,18 +1,21 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
 use crate::routes::{greet, health_check, subscribe};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    let connection = web::Data::new(connection);
     let port = listener.local_addr().unwrap().port();
     println!("listening at http://127.0.0.1:{}", port);
-    let server: Server = HttpServer::new(|| {
+    let server: Server = HttpServer::new(move || {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .route("/{name}", web::get().to(greet))
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
