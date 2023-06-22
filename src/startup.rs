aec69cd473a9320f98bcfaeac967a4cea15337f1
marcuses101 <mnjconnolly@gmail.com>
2023-06-22 08:11:33 -0400

    feat: configure loggings

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,6 @@
 use actix_web::dev::Server;
 use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -11,7 +10,6 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = web::Data::new(db_pool);
     let port = listener.local_addr().unwrap().port();
     println!("listening at http://127.0.0.1:{}", port);
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let server: Server = HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
