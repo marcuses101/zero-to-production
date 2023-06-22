d1fe3fed932e18b2c43f182cc3b04795e473a81c
marcuses101 <mnjconnolly@gmail.com>
2023-06-22 07:49:38 -0400

    feat: add logging

--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,7 @@
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -9,8 +11,10 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = web::Data::new(db_pool);
     let port = listener.local_addr().unwrap().port();
     println!("listening at http://127.0.0.1:{}", port);
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let server: Server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
